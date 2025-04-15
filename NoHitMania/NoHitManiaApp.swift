11e6d4ec680349fd17102814b3c7e8f3e8168e63
JustNekoChris <chbarajas20@gmail.com>
2025-04-15 14:49:46 -0700

    Initial Commit

---
+++ NoHitMania/NoHitManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoHitManiaApp.swift
+//  NoHitMania
+//
+//  Created by Christian Barajas on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct NoHitManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
